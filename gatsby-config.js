9800f4a312199c4a365cc07ad88fea58124e91d6
Santiago Homes <129601128+SantiagoHomes@users.noreply.github.com>
2023-04-04 11:33:17 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `santiago-homes-portfolio.netlify.app/`,
     // Your Name
     name: 'Santiago Homes',
     // Main Site Title
